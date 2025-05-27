7a5c3b70b546c31988c132b5708d7d4c2fc1c720
FatherOfGloom <antonuh2002@gmail.com>
2025-05-27 23:23:24 +0300

    Entry display + Goto parent folder + execute entry with default program

--- src/rover/mod.rs
+++ src/rover/mod.rs
@@ -36,7 +36,7 @@ impl Rect {
 
 #[derive(Default)]
 pub struct Rover {
-    current_path: PathBuf,
+    current_path: Option<PathBuf>,
     entries: Option<Vec<DirEntry>>,
     should_exit: bool,
     pivot: usize,
@@ -52,13 +52,13 @@ impl Rover {
 
         Ok(Rover {
             entries: Some(Self::read_dir(path)?),
-            current_path: path.to_path_buf(),
+            current_path: Some(path.to_path_buf()),
             console_screen: Rect::new(0, 0, w as usize, h as usize),
             ..Default::default()
         })
     }
 
-    pub fn update(&mut self) -> io::Result<()> {
+    pub fn update(&mut self) -> Result<(), String> {
         self.read_console_input()?;
         Ok(())
     }
@@ -68,6 +68,10 @@ impl Rover {
         stdout.queue(MoveTo(0, 0))?;
         stdout.flush()?;
         Ok(())
+    }
+
+    fn current_path_ref(&self) -> &Path {
+        self.current_path.as_ref().unwrap()
     }
 
     pub fn draw_entries(&self) -> io::Result<()> {
@@ -81,22 +85,23 @@ impl Rover {
         stdout.queue(Clear(terminal::ClearType::All))?;
         stdout.queue(MoveTo(0, 0))?;
 
-        stdout.write(format!("DIR: {}", self.current_path.display()).as_bytes())?;
+        stdout.write(format!("DIR: {}", self.current_path_ref().display()).as_bytes())?;
         stdout.queue(MoveTo(0, 1))?;
+
+        let mode_label = self.mode.to_string();
+
+        stdout.queue(MoveTo(
+            (self.console_screen.w - mode_label.len()) as u16, 0
+        ))?;
+
+        stdout.write(mode_label.as_bytes())?;
 
         for (i, e) in entries.iter().enumerate() {
             let prefix = if i == self.pivot { ">" } else { " " };
-            let dir_entry_path = format!("{} {}", prefix, e.path().display());
+            let dir_entry_path = format!("{} {}", prefix, e.file_name().to_str().unwrap());
             stdout.queue(MoveTo(0, i as u16 + 3))?;
             stdout.write(dir_entry_path.as_bytes())?;
         }
-
-        stdout.queue(MoveTo(
-            (self.console_screen.w - 10) as u16,
-            (self.console_screen.h) as u16,
-        ))?;
-
-        stdout.write(self.mode.to_string().as_bytes())?;
 
         stdout.flush()?;
 
@@ -105,7 +110,7 @@ impl Rover {
         Ok(())
     }
 
-    fn read_console_input(&mut self) -> std::io::Result<()> {
+    fn read_console_input(&mut self) -> Result<(), String> {
         match read().unwrap() {
             Event::Key(event) => {
                 if event.kind != KeyEventKind::Press {
@@ -119,6 +124,7 @@ impl Rover {
                                 'q' => self.should_exit = true,
                                 'c' => self.mode = RoverMode::COMMAND,
                                 'f' => self.mode = RoverMode::FLOW,
+                                'k' => self.execute_entry()?,
                                 _ => {}
                             }
                         } else {
@@ -131,14 +137,8 @@ impl Rover {
                     }
                     KeyCode::Up => self.shift(RoverDirection::UP),
                     KeyCode::Down => self.shift(RoverDirection::DOWN),
-                    KeyCode::Enter => {
-                        let selected_path = self.entries.as_ref().unwrap().get(self.pivot).unwrap().path();
-                        if selected_path.is_dir() {
-                            self.entries = Some(Self::read_dir(&selected_path).unwrap());
-                            self.current_path = selected_path;
-                            self.pivot = 0;
-                        }
-                    }
+                    KeyCode::Enter => self.execute_entry()?,
+                    KeyCode::Esc => self.to_parent_entry()?,
                     _ => {}
                 }
             }
@@ -147,6 +147,54 @@ impl Rover {
             }
             _ => {}
         }
+        Ok(())
+    }
+
+    fn entries_ref(&self) -> &Vec<DirEntry> {
+        self.entries.as_ref().unwrap()
+    }
+
+    fn execute_entry(&mut self) -> Result<(), String> {
+        let selected_path = self.entries_ref().get(self.pivot).unwrap().path();
+
+        if selected_path.is_dir() {
+            self.goto(&selected_path)?;
+        } else {
+            opener::open(selected_path.display().to_string())
+                .map_err(|e| format!("Error opening the file '{}': {}", selected_path.display(), e))?;
+        }
+
+        Ok(())
+    }
+
+    fn to_parent_entry(&mut self) -> Result<(), String> {
+        // Disgusting borrow checker doesn't let me borrow self.current_path.ancestors() directly
+        // Bc the iterator borrows self immutably so we have to wrap it into Option and take() it i guess, dont want to clone for no reason
+        let current_path = self.current_path.take().unwrap();
+
+        let mut cur_ancestors = current_path.ancestors();
+        // advance the iterator once since it returns current path
+        cur_ancestors.next().ok_or(format!("Current path '{}' is invalid.", current_path.display()))?;
+
+        let Some(parent_entry) = cur_ancestors.next() else {
+            self.current_path = Some(current_path);
+            return Err("Cannot move back from root folder.".to_string());
+        };
+
+        self.goto(parent_entry).map_err(|e| { self.current_path = Some(current_path); e })?;
+
+        Ok(())
+    }
+
+    fn goto(&mut self, selected_path: &Path) -> Result<(), String> {
+        if !selected_path.exists() {
+            return Err(format!("Chosen folder '{}' doesn't exist.", selected_path.display()));
+        }
+
+        self.entries = Some(Self::read_dir(&selected_path).unwrap());
+        self.current_path = Some(selected_path.to_path_buf());
+        self.pivot = 0;
+
         Ok(())
     }
 
@@ -176,12 +224,12 @@ impl Rover {
 
     fn read_dir(path: &Path) -> Result<Vec<DirEntry>, String> {
         if !path.exists() {
-            return Err(format!("Given path doesn't exist: '{}'", path.display()));
+            return Err(format!("Given path doesn't exist: '{}'.", path.display()));
         }
 
         if !path.is_dir() {
             return Err(format!(
-                "Given path is not a directory: '{}'",
+                "Given path is not a directory: '{}'.",
                 path.display()
             ));
         }
